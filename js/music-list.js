0013869691dd8a5c30be0b9ed1a78138482f55f8
kaboria <kaporyakak@gmail.com>
2024-01-29 18:39:42 +0200

    here i made simple music player with js

---
+++ js/music-list.js
@@ -0,0 +1,18 @@
+
+let allMusic = [
+
+  {
+    name: "Lost in the City Lights",
+    artist: "Cosmo Sheldrake",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Forest Lullaby",
+    artist: "Lesfm",
+    img: "music-2",
+    src: "music-2"
+  }
+
+];
+
---
+++ js/script.js
@@ -0,0 +1,85 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+mainAudio = wrapper.querySelector("#main-audio")
+
+
+let musicIndex =  Math.floor((Math.random() * allMusic.length) + 1 );
+isMusicPaused = true;
+ 
+window.addEventListener("load", ()=>{
+    loadMusic(musicIndex);
+
+});
+
+
+function loadMusic(indexNumb){
+  musicName.innerText = allMusic[indexNumb  - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].src}.png`;
+  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+
+}
+
+
+//play music function
+function playMusic(){
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+//pause music function
+function pauseMusic(){
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+//prev music function
+function prevMusic(){
+  musicIndex--; //decrement of musicIndex by 1
+  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+
+}
+
+
+
+//next music function
+function nextMusic(){
+  musicIndex++; //increment of musicIndex by 1
+  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+
+}
+
+// play or pause button event
+playPauseBtn.addEventListener("click", ()=>{
+  const isMusicPlay = wrapper.classList.contains("paused");
+
+ //if isPlayMusic is true then call pauseMusic else call playMusic
+ isMusicPlay ? pauseMusic() : playMusic();
+
+});
+
+
+
+
+//prev music button event
+prevBtn.addEventListener("click", ()=>{
+  prevMusic();
+});
+
+//next music button event
+nextBtn.addEventListener("click", ()=>{
+  nextMusic();
+});
